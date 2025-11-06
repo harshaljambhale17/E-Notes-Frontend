d046e3eeed33db6cf1a3c510784176583459dc4c
harshaljambhale17 <harshaljambhale17@gmail.com>
2025-11-06 11:59:38 +0530

    Update the env variable name

--- src/config/AxiosHelper.js
+++ src/config/AxiosHelper.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-export const baseURL = import.meta.env.REACT_APP_API_BASE_URL
+export const baseURL = import.meta.env.VITE_API_BASE_URL
 
 export const httpClient = axios.create({
     baseURL: baseURL,
